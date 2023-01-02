9a51e3b072025962b3ee7f95e4722b6ce9c29a0d
stivin-00 <68236676+stivin-00@users.noreply.github.com>
2023-01-02 11:59:48 +0100

    hello

--- components/Thumbnails.tsx
+++ components/Thumbnails.tsx
@@ -10,15 +10,15 @@ interface Props {
 }
 
 function Thumbnail({ movie }: Props) {
-//   const [currentMovie, setCurrentMovie] = useRecoilState(movieState);
-//   const [showModal, setShowModal] = useRecoilState(modalState);
+  const [currentMovie, setCurrentMovie] = useRecoilState(movieState);
+  const [showModal, setShowModal] = useRecoilState(modalState);
 
   return (
     <div
       className={`relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105`}
       onClick={() => {
-        // setCurrentMovie(movie);
-        // setShowModal(true);
+        setCurrentMovie(movie);
+        setShowModal(true);
       }}
     >
       <Image
